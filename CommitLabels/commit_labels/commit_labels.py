294f7760a7e5df99247633c8f4502dcc0716a937
Steve Plaza <plazas@janelia.hhmi.org>
2014-03-25 14:21:27 -0400

    cleans up some errors; actual write disabled

--- CommitLabels/commit_labels/commit_labels.py
+++ CommitLabels/commit_labels/commit_labels.py
@@ -2,16 +2,22 @@ import argparse
 import h5py
 import numpy
 import struct
+import json
+import requests
 
 def execute(argv):
-    parser = ArgumentParser.argparse(description="Writes h5 to DVID")
+    parser = argparse.ArgumentParser(description="Writes h5 to DVID")
     parser.add_argument('config_file', type=str, help="Location of configuration json")
     args = parser.parse_args()
 
     json_data = json.load(open(args.config_file))
 
-    hfile = h5py.File(json_data["labels.h5"], 'r')
+    hfile = h5py.File(json_data["labels"], 'r')
     labels = numpy.array(hfile['stack']).astype(numpy.uint64)
+
+    # crop labels
+    bufsz = json_data["border"] + 10
+    labels = labels[bufsz:-1*bufsz, bufsz:-1*bufsz, bufsz:-1*bufsz]
 
     # remapping is based off of an adjusted set of labels (if necessary)
     if json_data["offset"] != 0:
@@ -30,17 +36,20 @@ def execute(argv):
         labels = vectorized_relabel(labels).astype(numpy.uint64)
 
     # write dvid volume
-    # <server>/api/node/<UUID>/<labelname>/0_1_2/
+    # <server>/api/node/<UUID>/<labelname>/raw/0_1_2/
     write_location = json_data["write-location"] 
     bbox1 = json_data["bbox1"]
     bbox2 = json_data["bbox2"]
 
     sizes = [i - j for i, j in zip(bbox2, bbox1)]
-    write_location += "{sx}_{sy}_{sz}/{x}_{y}_{z}".format(sx=sizes[0],
+    write_location += "/{sx}_{sy}_{sz}/{x}_{y}_{z}".format(sx=sizes[0],
             sy=sizes[1], sz=sizes[2], x=bbox1[0], y=bbox1[1], z=bbox1[2])
 
+    labels = labels.ravel().copy()
     labels_data = '<' + 'Q'*len(labels)
     labels_bin = struct.pack(labels_data, *labels)
-    requests.post(write_location, data=labels_bin,
-            headers={'content-type': 'application/octet-stream'}) 
+    
+    print "Send when post is fixed"
+    #requests.post(write_location, data=labels_bin,
+    #        headers={'content-type': 'application/octet-stream'}) 
 
